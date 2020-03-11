588a3773e07e223f787438aea30b03aa79bdd008
standeman1611 <stanhgroen@gmail.com>
2020-03-11 17:36:02 +0100

    added to main

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
-Glazen = int(input("Hoeveel glazen heb je gehad?\n"))
-Massa = float(input("Wat is je gewicht?\n"))
-Tijd = float(input("Hoelang geleden was je laatste glas alcohol? (in uren)\n"))
-Gender = input("Wat is je geslacht?\n")
-Vocht = 0
+from tkinter import *
+import tkinter as tk
 
-if Gender.lower() == "man":
-    Vocht = 0.72
-elif Gender.lower() == "vrouw":
-    Vocht = 0.61
+window = tk.Tk()
 
+tk.Label(window, text="Alcohol promillage calculator").grid()
 
-a = Glazen*10
-b = Massa*Vocht
-c = Tijd-0.5
-d = Massa*0.002
-e = a/b
+tk.Label(window, text="Hoeveel glazen heb je gehad?").grid(row=1)
 
-Promille = e-c*d
+g = tk.Entry(window)grid(row=1, column=1)
 
-print(Promille,"‰")
+tk.Label(window, text="Wat is je gewicht? (in kg)").grid(row=2)
 
-print(((Glazen*10)/(Massa*Vocht))-((Tijd-0.5)*(Massa*0.002)))
+m = tk.Entry(window).grid(row=2, column=1)
+
+tk.Label(window, text="Hoelang geleden was je laatste glas alcohol? (in uren)").grid(row=3)
+
+t = tk.Entry(window).grid(row=3, column=1)
+
+tk.Label(window, text="Wat is je geslacht?").grid(row=4)
+
+ge = tk.Entry(window).grid(row=4, column=1)
+
+Glazen = g.get()
+Massa = m.get()
+Tijd = t.get()
+Gender = ge.get()
+
+def calc():
+    if Gender.lower() == "man":
+        Vocht = 0.72
+    elif Gender.lower() == "vrouw":
+        Vocht = 0.61
+    promille = round(((Glazen * 10) / (Massa * Vocht)) - ((Tijd - 0.5) * (Massa * 0.002)), 1)
+    tk.Label(window, text=promille).grid(row=5)
+
+b = tk.Button(window, text="Calculate", command=calc)
+b.grid(row=6)
+
+window.mainloop()
